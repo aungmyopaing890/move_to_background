23bfc1bb420b579e217fb793030d0ea3acb9f40c
aungmyopaing <62334147+aungmyopaing890@users.noreply.github.com>
2025-02-20 21:59:11 +0700

    update gradle and something

--- lib/move_to_background.dart
+++ lib/move_to_background.dart
@@ -6,8 +6,9 @@ import 'package:flutter/services.dart';
 /// A class containing the static function used.
 class MoveToBackground {
   /// The method channel used to contact the native side
-  static const MethodChannel _channel =
-      const MethodChannel('move_to_background');
+  static const MethodChannel _channel = const MethodChannel(
+    'move_to_background',
+  );
 
   /// Calls the platform-specific function to send the app to the background
   static Future<void> moveTaskToBack() async {
